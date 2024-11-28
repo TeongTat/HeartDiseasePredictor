b1ef98ca591d60fdbf1f4592e458ea7209737f19
TeongTat <102958559+TeongTat@users.noreply.github.com>
2024-11-28 13:29:19 +0800

    Update 01_userguide.py

--- navigation/01_userguide.py
+++ navigation/01_userguide.py
@@ -1,1 +1,13 @@
 import streamlit as st
+import requests
+import streamlit.components.v1 as components
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+import PIL
+from PIL import Image
+
+st.tittle("User Guide")
+
+st.write(
+  """ This page will gives some idea to user how to fill up """)
