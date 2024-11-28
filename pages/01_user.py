ccb2a8c3126db6e0d491b2b48b338c78b05548ef
TeongTat <102958559+TeongTat@users.noreply.github.com>
2024-11-28 15:05:56 +0800

    Update 01_user.py

--- pages/01_user.py
+++ pages/01_user.py
@@ -12,26 +12,34 @@ st.title("User Guide")
 st.write(
   """ This page will gives guidance to user some information prior to using the Heart Disease Predictor""")
 
-#write description:
-st.write("User can find fill up the information below to obtain the BMI for the predictor.")
 
 # Embed the Adult BMI Calculator in the Streamlit app
 st.subheader("Adult BMI Calculator")
 components.iframe("https://www.cdc.gov/bmi/adult-calculator/calculator.html", height=640,scrolling=True)
 
+#write description:
+st.write("User can find fill up the information below to obtain the BMI for the predictor.")
 
 # Display the Blood Pressure image and source
+st.subheader("Blood Pressure: Showing Systolic and Diastolic")
 image_path_bp = "bloodpressure.jpg"
 st.image(Image.open(image_path_bp), caption="Blood Pressure Chart", use_column_width=True)
 st.write("Source: [Heart Org](https://www.heart.org/en/health-topics/high-blood-pressure/understanding-blood-pressure-readings)")
 
 
 # Display the Cholesterol Level image and source
+st.subheader("Cholesterol Level")
 image_path_chol = "Cholesterol level.webp"
 st.image(Image.open(image_path_chol), caption="Cholesterol Level Chart", use_column_width=True)
 st.write("Source: [Medicine Marketplace](https://medicinemarketplace.com/controlling-cholestrol/)")
+/
+/
+st.write("Level 1:Normal, Level 2:Borderline, Level 3: High")
 
 # Display the Glucose Level image and source
 image_path_glucose = "Glucose_level.webp"
 st.image(Image.open(image_path_glucose), caption="Glucose Level Chart", use_column_width=True)
 st.write("Source: [CMI Health](https://www.cmihealth.com/blogs/news/what-your-blood-glucose-test-results-mean-cmi-health-blog)")
+/
+/
+st.write("Level 1:Normal, Level 2:Borderline, Level 3: High")
