bcf2b64329df7479201f0d7a3de96e003520ea92
TeongTat <102958559+TeongTat@users.noreply.github.com>
2024-11-28 14:51:25 +0800

    Update 01_user.py

--- pages/01_user.py
+++ pages/01_user.py
@@ -23,10 +23,10 @@ components.iframe("https://www.cdc.gov/bmi/adult-calculator/calculator.html", he
 
 # Display the Blood Pressure image and source
 try:
-image_path_bp = "Bloodpressure.png"
-st.image(Image.open(image_path_bp), caption="Blood Pressure Chart", use_column_width=True)
-st.write("Source: [Healthline](https://www.healthline.com/health/blood-pressure-chart)")
-  except FileNotFoundError:
+  image_path_bp = "Bloodpressure.png"
+  st.image(Image.open(image_path_bp), caption="Blood Pressure Chart", use_column_width=True)
+  st.write("Source: [Healthline](https://www.healthline.com/health/blood-pressure-chart)")
+except FileNotFoundError:
   st.error("The image file was not found. Please check the file path.")
 st.write("Source: [Healthline](https://www.healthline.com/health/blood-pressure-chart)")
 
