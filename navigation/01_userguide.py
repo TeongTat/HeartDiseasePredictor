48a5ed789ad43923b8dd0d49352dec00a093cc06
TeongTat <102958559+TeongTat@users.noreply.github.com>
2024-11-28 13:36:24 +0800

    Update 01_userguide.py

--- navigation/01_userguide.py
+++ navigation/01_userguide.py
@@ -11,3 +11,17 @@ st.tittle("User Guide")
 
 st.write(
   """ This page will gives some idea to user how to fill up """)
+
+
+
+  # Load an image from a local file
+    image_bp = "Bloodpressure.png"
+st.write("Source: https://www.healthline.com/health/blood-pressure-chart")
+
+ # Load an image from a local file
+    image_cholesterol = "Cholesterol level.webp"
+st.write("Source: https://medicinemarketplace.com/controlling-cholestrol/")
+
+# Load an image from a local file
+  image_glucose = "Glucose_level.webp"
+st.write("Source: https://www.cmihealth.com/blogs/news/what-your-blood-glucose-test-results-mean-cmi-health-blog")
