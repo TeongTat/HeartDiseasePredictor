d3d1eaba24f59fe843504ec404ad21136752b838
TeongTat <102958559+TeongTat@users.noreply.github.com>
2024-12-01 12:53:11 +0800

    Update 03_Visualization on USA Heart Disease.py

--- pages/03_Visualization on USA Heart Disease.py
+++ pages/03_Visualization on USA Heart Disease.py
@@ -4,9 +4,11 @@ st.title("USA Heart Disease Visualization")
 
 # Embed the USA Visualization in the Streamlit app
 st.subheader("Visualization on USA Heart Disease example")
-cdc_widget_code = """<div data-cdc-widget="CountyMapsTemplate" data-instance-name="HeartDiseaseAndStroke" data-default-state="Alabama" data-default-dataset="Heart Disease"></div> <script src="https://tools.cdc.gov/1M1B"></script>
+cdc_widget_code = """
+<div data-cdc-widget="CountyMaps" data-instance-name="HeartDiseaseAndStroke"></div>
+<br/>
+<script src="https://tools.cdc.gov/1M1B"></script>
 """
 
 # Use st.components.v1.html to render the widget
 st.components.v1.html(cdc_widget_code, height=600, scrolling=True)
-
