6e6f5f11f7f64ed65d68ac595705d7cd5db64575
TeongTat <102958559+TeongTat@users.noreply.github.com>
2024-12-01 13:03:51 +0800

    Update 03_Visualization on USA Heart Disease.py

--- pages/03_Visualization on USA Heart Disease.py
+++ pages/03_Visualization on USA Heart Disease.py
@@ -4,7 +4,7 @@ st.title("USA Heart Disease Visualization")
 
 # Embed the USA Visualization in the Streamlit app
 st.subheader("Visualization on USA Heart Disease example")
-iframe_code = """
+iframe_cdc = """
 <iframe src="https://tools.cdc.gov/api/v2/resources/media/CountyMapsTemplate?default-state=Florida&default-dataset=Heart%20Disease" 
         width="100%" 
         height="700" 
