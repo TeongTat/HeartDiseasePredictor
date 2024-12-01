01247b1b90c4e5460f187fd855b757ea6d8cf1d4
TeongTat <102958559+TeongTat@users.noreply.github.com>
2024-12-01 12:35:55 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -6,7 +6,10 @@ st.title("Heart Disease Prediction")
 st.subheader("This is an example case study using Heart Disease Prediction to create web app")
 
 st.write(
-  """ The dataset is obtained from Kaggle: https://www.kaggle.com/datasets/sulianova/cardiovascular-disease-dataset .
-  Upon obtaining the data, the data science methodolgy is applied to create the modelling.
-  Data cleaning includes null value identification is processed. We added a new column which is BMI as convertion from height and weight from the dataset.
-  The following infographic will provide useful information prior using the Heart Disease Preditor""")
+  """ The dataset is obtained from Kaggle: https://www.kaggle.com/datasets/sulianova/cardiovascular-disease-dataset .""")
+st.write(
+  """Upon obtaining the data, the data science methodolgy is applied to create the modelling.
+  - Data cleaning includes null value identification is processed.
+  - We added a new column which is BMI as convertion from height and weight from the dataset.
+  - On the top left there is " > " which user can click to go to different pages.
+  - The pages will provide some guidance and also the prediction for user to use. """)
